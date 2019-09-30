462a6bd480f67b39a6bf11d4f5b8811d7b319056
Ted Tran <TedTran2019@gmail.com>
2019-09-29 23:51:37 -0700

    Completed ActiveRecord Lite! Finished up association functionality

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,41 +10,92 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    single = name.to_s.singularize
+    @foreign_key ||= "#{single}_id".to_sym
+    @class_name ||= single.capitalize
+    @primary_key ||= :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+    @foreign_key ||= "#{self_class_name.underscore}_id".to_sym
+    @class_name ||= name.to_s.singularize.capitalize
+    @primary_key ||= :id
   end
 end
 
+=begin
+  YOU ARE: Person class
+  belongs_to :house, 
+  foreign_key: :house_id, 
+  primary_key: :id,
+  class_name: "House"
+
+  YOU ARE: House class
+  has_many :people,
+  foreign_key: :house_id, 
+  primary_key: :id,
+  class_name: "Person"
+=end
+
 module Associatable
   # Phase IIIb
+  # belongs_to is a class method through extend Module that uses 
+  # define_method to create instance methods
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
+    define_method(name) { 
+      results = DBConnection.execute(<<-SQL, self.send(options.foreign_key))
+        SELECT
+          #{options.table_name}.*
+        FROM
+          #{options.table_name}
+        WHERE
+          #{options.primary_key} = ?
+      SQL
+      results.empty? ? nil : options.model_class.new(results.first)
+    }
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) {
+      results = DBConnection.execute(<<-SQL, self.send(options.primary_key))
+        SELECT
+          #{options.table_name}.*
+        FROM
+          #{options.table_name}
+        WHERE
+          #{options.foreign_key} = ?
+      SQL
+      results.map { |result| options.model_class.new(result) }
+    }
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @options ||= {}
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
--- active_record_lite/lib/04_associatable2.rb
+++ active_record_lite/lib/04_associatable2.rb
@@ -1,10 +1,37 @@
 require_relative '03_associatable'
 
 # Phase IV
+
+=begin
+YOUR CLASS: Cat
+has_one_through :home,
+through: :human, # cat has a human association, can look in assoc_options 
+source: :house   # cat doesn't have house assoc--human does.
+=end
 module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) {
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      target = source_options.table_name
+      from = through_options.table_name
+      result = DBConnection.execute(<<-SQL, self.send(through_options.foreign_key))
+        SELECT
+          #{target}.*
+        FROM
+          #{from}
+        JOIN
+          #{target} ON #{from}.#{source_options.foreign_key} = #{target}.#{source_options.primary_key}
+        WHERE
+          #{from}.#{through_options.primary_key} = ?
+      SQL
+      result.empty? ? nil : source_options.model_class.new(result.first)
+    }
+
+    # define_method(name) {
+    #   self.send(through_name).send(source_name)
+    # }
   end
 end
